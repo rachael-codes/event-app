cb465a1ece72f1159281e13ad488cc76194faa11
Rachael <rachaelfavour2005@gmail.com>
2023-08-29 18:55:52 +0100

    first commit

---
+++ models/attendeeModel.js
@@ -0,0 +1,26 @@
+// models/attendeeModel.js
+const db = require('../config/db'); // Import the database configuration
+
+// Register an attendee
+const registerAttendee = (name, email, callback) => {
+  const sql = 'INSERT INTO attendees (name, email) VALUES (?, ?)';
+  db.query(sql, [name, email], (err, result) => {
+    if (err) {
+      return callback(err, null);
+    }
+    callback(null, { id: result.insertId, name, email });
+  });
+};
+
+// Get list of all attendees
+const getAllAttendees = (callback) => {
+  const sql = 'SELECT * FROM attendees';
+  db.query(sql, (err, rows) => {
+    if (err) {
+      return callback(err, null);
+    }
+    callback(null, rows);
+  });
+};
+
+module.exports = { registerAttendee, getAllAttendees };
